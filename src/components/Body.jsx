bdf0cc39b63d50a2fbd8e55d845a64f628ef5407
Bhavya <Bhavyagupta2302@gmail.com>
2025-11-04 16:54:37 +0530

    Unauthorized user gets redirected to login page

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,37 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
+import axios from "axios";
 
-const body = () => {
+const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const userData = useSelector((store) => store.user);
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const user = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+
+      dispatch(addUser(user.data));
+    } catch (error) {
+      if (error.status === 401) {
+        navigate("/login");
+      }
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <NavBar />
@@ -12,4 +41,4 @@ const body = () => {
   );
 };
 
-export default body;
+export default Body;
